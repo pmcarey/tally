31d0fb6a02ac84f8d5b90ff1428e43aa72fad889
michelalbert <michelalbert198213@mail.com>
2017-01-26 15:12:11 +0800

    login-signup

---
+++ client/main.js
@@ -0,0 +1,1 @@
+import '../imports/ui/login.js';
---
+++ imports/ui/signup.js
@@ -0,0 +1,19 @@
+import { Template } from 'meteor/templating';
+import { Tasks } from '../api/tasks.js';
+import "../assets/css/font-awesome.min.css";
+import "../assets/css/simple-line-icons.min.css";
+import "../assets/css/bootstrap.min.css";
+import "../assets/css/uniform.default.css";
+import "../assets/css/bootstrap-switch.min.css";
+import "../assets/css/select2.min.css";
+import "../assets/css/select2-bootstrap.min.css";
+import "../assets/css/components.min.css";
+import "../assets/css/plugins.min.css";
+import "../assets/css/login-3.min.css";
+import './signup.html';
+ 
+Template.body.helpers({
+  tasks() {
+    return Tasks.find({});
+  },
+});
